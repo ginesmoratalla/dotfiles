8559639786d92254477557a5f19a13ddd23b775b
ginesmoratalla <ginesmoratalla@gmail.com>
2025-07-10 18:48:27 +0200

    add nix lspconfig

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -5,50 +5,38 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup ({})
-      lspconfig.pyright.setup ({})
-      lspconfig.clangd.setup ({})
-      lspconfig.lua_ls.setup ({})
-      lspconfig.jdtls.setup ({})
-      lspconfig.ast_grep.setup ({})
-      lspconfig.jsonls.setup ({})
-      lspconfig.rust_analyzer.setup ({})
-      lspconfig.vuels.setup ({})
-      lspconfig.htmx.setup ({})
-      lspconfig.ltex.setup ({})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.nixd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.zls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ccls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.diagnostic.config({ virtual_text = true })
-
 		end,
 	},
-	-- MASON
-	{
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-	},
-	{
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "clangd",
-          "pyright",
-          "lua_ls",
-          "jdtls",
-          "ast_grep",
-          "ltex",
-          "jsonls",
-          "rust_analyzer",
-          "vuels",
-          "htmx",
-        }})
-      end
-	},
 }
-
